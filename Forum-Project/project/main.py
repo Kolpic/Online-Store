f6785629947f34d0e846cf1df2ab135f1ace2b0b
Kolpic <galincho112@gmail.com>
2024-03-07 09:37:46 +0200

    changed logic in main.py, bug fixes

--- Forum-Project/project/main.py
+++ Forum-Project/project/main.py
@@ -26,24 +26,28 @@ user = config.user
 password = config.password
 host = config.host
 
-conn = psycopg2.connect(dbname=database, user=user, password=password, host=host)
-
 @app.route('/registration', methods =['GET', 'POST'])
 def registration():
+
+    if request.method == 'GET':
+        return render_template('registration.html')
+    
     if request.method == 'POST':
+        conn = psycopg2.connect(dbname=database, user=user, password=password, host=host)
+
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         email = request.form['email']
-        password = request.form['password']
+        password_ = request.form['password']
 
-        hashed_password = hash_password(password)
+        hashed_password = hash_password(password_)
         verification_code = os.urandom(24).hex()
         try:
             if len(first_name) < 3 or len(first_name) > 50:
                 raise exception.CustomError('First name is must be between 3 and 50 symbols')
             if len(last_name) < 3 or len(last_name) > 50:
                 raise exception.CustomError('Last name must be between 3 and 50 symbols')
-        
+            
             regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
             if not (re.fullmatch(regex, email)):
@@ -60,7 +64,6 @@ def registration():
         send_verification_email(email, verification_code)
 
         return redirect(url_for('verify'))
-    return render_template('registration.html')
 
 def send_verification_email(user_email, verification_code):
     with app.app_context():
@@ -76,16 +79,26 @@ def hash_password(password):
 
 @app.route('/verify', methods=['POST', 'GET'])
 def verify():
+
+    if request.method == 'GET':
+        return render_template('verify.html')
+
     if request.method == 'POST':
+        conn = psycopg2.connect(dbname=database, user=user, password=password, host=host)
+
         email = request.form['email']
         verification_code = request.form['verification_code']
 
         cur = conn.cursor()
 
         cur.execute("SELECT email FROM users WHERE email = %s", (email,))
+        
+        dictionary = {}
 
         try:
-            email_from_database = cur.fetchone()[0]
+            # cur.fetchone()[0]
+            # email_from_database 
+            email_from_database = cur.fetchone()[0]   
             if email_from_database == email:
                 cur.execute("UPDATE users SET verification_status = true WHERE verification_code = %s", (verification_code,))
                 conn.commit()
@@ -94,14 +107,21 @@ def verify():
         except TypeError as e:
             error_message = 'You entered different email'
             session['verification_error'] = str(error_message)
-            return redirect(url_for('verify'))   
-    return render_template('verify.html')  
+            return redirect(url_for('verify'))    
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    conn = psycopg2.connect(dbname=database, user=user, password=password, host=host)
+
+    if request.method == 'GET':
+        return render_template('login.html')
+
     if request.method == 'POST':
+
+        conn = psycopg2.connect(dbname=database, user=user, password=password, host=host)
+
         email = request.form['email']
-        password = request.form['password']
+        password_ = request.form['password']
 
         cur = conn.cursor()
 
@@ -111,20 +131,24 @@ def login():
         cur.execute("SELECT password FROM users WHERE email = %s", (email,))
         hashed_password = cur.fetchone()[0]
 
-        are_passwords_same = bool(verify_password(password, hashed_password))
+        are_passwords_same = bool(verify_password(password_, hashed_password))
 
-        if are_passwords_same:
-            if is_the_email_verified:
-                session['user_email'] = email   
-                return redirect(url_for('home'))
-            else:
-               error_message = 'Your account is not verified or has been deleted' 
-        else:
+        flag = False
+        if not are_passwords_same:
             error_message = 'Invalid email or password'
+            flag = True
 
-        session['login_error'] = str(error_message)
-        return redirect(url_for('login')) 
-    return render_template('login.html')
+        if not is_the_email_verified:
+            error_message = 'Your account is not verified or has been deleted'
+            flag = True
+
+        if flag:
+            session['login_error'] = str(error_message)
+            return redirect(url_for('login')) 
+        
+        session['user_email'] = email   
+        return redirect(url_for('home'))
+
 
 def verify_password(plain_password, hashed_password):
     return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
@@ -134,12 +158,12 @@ def is_authenticated():
 
 @app.route('/home', methods=['GET'])
 def home():
-    if is_authenticated():
-        return render_template('home.html')
-    else:
+    if not is_authenticated():
         return redirect(url_for('login'))
     
-@app.route('/logout', methods=['GET'])
+    return render_template('home.html')
+
+@app.route('/logout', methods=['GET'])    
 def logout():
     session.pop('user_email', None) 
     return redirect(url_for('home'))
@@ -148,53 +172,74 @@ def logout():
 def settings():
     if 'user_email' not in session:
         return redirect(url_for('login'))
+    
     return render_template('settings.html')
 
 @app.route('/update_settings', methods=['POST'])
 def update_settings():
-    if 'user_email' in session:
-        
-        first_name = request.form['first-name']
-        last_name = request.form['last-name']
-        email = request.form['email']
-        password = request.form['password']
+    if 'user_email' not in session:
+         return 'Unauthorized', 401
+    
+    conn = psycopg2.connect(dbname=database, user=user, password=password, host=host)
 
-        cur = conn.cursor()
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    email = request.form['email']
+    password_ = request.form['password']
 
-        if not first_name == '' and not last_name == '' and not email == '' and not password == '':
-            cur.execute("UPDATE users SET first_name = %s, last_name = %s,email = %s, password = %s WHERE email = %s", (first_name, last_name, email, password, session['user_email']))
-        elif first_name:
-            cur.execute("UPDATE users SET first_name = %s WHERE email = %s", (first_name, session['user_email']))    
-        elif last_name:
-            cur.execute("UPDATE users SET last_name = %s WHERE email = %s", (last_name, session['user_email']))
-        elif email:
-            cur.execute("UPDATE users SET email = %s WHERE email = %s", (email, session['user_email']))
-        elif password:
-            hashed_password = hash_password(password)
-            cur.execute("UPDATE users SET password = %s WHERE email = %s", (hashed_password, session['user_email']))
+    cur = conn.cursor()
         
-        conn.commit()
-        cur.close()
+    query_string = "UPDATE users SET "
+    fields_list = []
+
+    if first_name:
+        query_string += "first_name = %s, "
+        fields_list.append(first_name)
+    if last_name:
+        query_string += "last_name = %s, "
+        fields_list.append(last_name)
+    if email:
+        query_string += "email = %s, "
+        fields_list.append(email)
+    if password_:
+        query_string += "password = %s, "
+        hashed_password = hash_password(password_)
+        fields_list.append(hashed_password)
+
+    query_string = query_string[:-2]
+    query_string += " WHERE email = %s"
+    email_in_session = session['user_email']
+    fields_list.append(email_in_session)
+
+    cur.execute(query_string, (fields_list))
         
-        # Update session email if email was changed
+    conn.commit()
+    cur.close()
+        
+    # Update session email if email was changed
+    if email:
         session['user_email'] = email
         
-        return redirect(url_for('home'))
-    return 'Unauthorized', 401
+    return redirect(url_for('home'))
 
-@app.route('/delete_account', methods=['POST'])
+@app.route('/delete_account', methods=['POST', 'GET'])
 def delete_account():
-    if 'user_email' in session:
-        user_email = session['user_email']
-        
-        cur = conn.cursor()
-        cur.execute("UPDATE users SET verification_status = false WHERE email = %s", (user_email,))
-        conn.commit()
-        cur.close()
-        
-        session.clear()
-        return redirect(url_for('login'))
-    return 'Unauthorized', 401
+
+    if 'user_email' not in session:
+        return redirect(url_for('registration'))
+    
+    conn = psycopg2.connect(dbname=database, user=user, password=password, host=host)
+
+    user_email = session['user_email']
+    cur = conn.cursor()
+
+    cur.execute("UPDATE users SET verification_status = false WHERE email = %s", (user_email,))
+    conn.commit()
+
+    cur.close()
+    session.clear()
+
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
