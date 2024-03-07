1370a5a0ed88801f567dc848f0064ac36937bf4d
Kolpic <galincho112@gmail.com>
2024-03-07 17:42:22 +0200

    updated task square

--- Fast Tasks/Squares/squareV2.js
+++ Fast Tasks/Squares/squareV2.js
@@ -1,10 +1,12 @@
-function solveSudoku(n, grid, row, col)
+let set = new Set();
+let N;
+
+function prepareMAtrix(n, grid, row, col)
 {
-	N = n * n
+    N = n * n
 	
     let matrix = Array(n*n).fill(0).map(()=>Array(n*n).fill(0));
     let pointerMatrix = 0;
-    let set = new Set();
     
     for (let i = 0; i < n * n; i++) {
         for (let j = 0; j < n * n; j++) {
@@ -14,101 +16,71 @@ function solveSudoku(n, grid, row, col)
             pointerMatrix++;
         }
     }
-
     set.delete('0');
+    solveSudoku(matrix, row, col)
+}
 
-	if (row == N - 1 && col == N)
-		return true;
+function solveSudoku(matrix, row, col) {
+	if (row == N - 1 && col == N) {
+		return false;
+    }
 
-	if (col == N)
-	{
+	if (col == N) {
 		row++;
 		col = 0;
 	}
 
-	if (grid[row][col] != 0)
-		return solveSudoku(grid, row, col + 1);
+	if (matrix[row][col] != 0) {
+		return solveSudoku(matrix, row, col + 1);
+    }
 
-	for(let num of set.values)
-	{
-		
-		// Check if it is safe to place
-		// the num (1-9) in the given 
-		// row ,col ->we move to next column
-		if (isSafe(grid, row, col, num))
-		{
-			
-			/* assigning the num in the current
-			(row,col) position of the grid and
-			assuming our assigned num in the position
-			is correct */
-			grid[row][col] = num;
+	for(let num of set) {
+		if (isSafe(matrix, row, col, num)) {
+			matrix[row][col] = num;
 
-			// Checking for next
-			// possibility with next column
-			if (solveSudoku(grid, row, col + 1))
+			if (solveSudoku(matrix, row, col + 1)) {
 				return true;
+            }
 		}
-		
-		/* removing the assigned num , since our
-		assumption was wrong , and we go for next
-		assumption with diff num value */
-		grid[row][col] = 0;
+
+		matrix[row][col] = Number(0);
 	}
 	return false;
 }
 
-/* A utility function to print grid */
-function print(grid)
-{
-	for(let i = 0; i < N; i++)
-	{
-		for(let j = 0; j < N; j++)
-			document.write(grid[i][j] + " ");
-			
-		document.write("<br>");
-	}
-}
-
-// Check whether it will be legal
-// to assign num to the
-// given row, col
-function isSafe(grid, row, col, num)
-{
-	
-	// Check if we find the same num
-	// in the similar row , we
-	// return false
-	for(let x = 0; x <= 8; x++)
-		if (grid[row][x] == num)
+function isSafe(matrix, row, col, num) {
+	for(let x = 0; x < N; x++) {
+		if (matrix[row][x] == num) {
 			return false;
+        }
+    }
 
-	// Check if we find the same num
-	// in the similar column ,
-	// we return false
-	for(let x = 0; x <= 8; x++)
-		if (grid[x][col] == num)
-			return false;
+	for(let x = 0; x < N; x++){
+		if (matrix[x][col] == num) {
+            return false;
+        }
+    }
 
-	// Check if we find the same num
-	// in the particular 3*3
-	// matrix, we return false
-	let startRow = row - row % 3, 
-		startCol = col - col % 3;
+	let startRow = row - row % Math.sqrt(N), 
+		startCol = col - col % Math.sqrt(N);
 		
-	for(let i = 0; i < 3; i++)
-		for(let j = 0; j < 3; j++)
-			if (grid[i + startRow][j + startCol] == num)
-				return false;
-
+	for(let i = 0; i < Math.sqrt(N); i++) {
+		for(let j = 0; j < Math.sqrt(N); j++){
+			if (matrix[i + startRow][j + startCol] == num) {
+                return false; 
+            }
+        }				
+    }
 	return true;
 }
 
 let n = 2
-let grid = ["0 0 # $ 0 0 0 0 * 0 0 0 0 # 0 5"]
+let grid = "0 0 # $ 0 0 0 0 * 0 0 0 0 # 0 5"
 
-if (solveSudoku(n, grid, 0, 0))
+if (prepareMAtrix(n, grid, 0, 0)) {
+    let a = 5
 	print(grid)
-else
-	document.write("no solution exists ")
+} else {
+    document.write("no solution exists ")
+}
 
