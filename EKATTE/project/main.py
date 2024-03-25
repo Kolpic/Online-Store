697f53a147f68e1024a067a61a7b40d854ae0c2b
Kolpic <galincho112@gmail.com>
2024-03-25 12:30:38 +0200

    finished task ekatte

--- EKATTE/project/main.py
+++ EKATTE/project/main.py
@@ -23,9 +23,36 @@ HTML_TEMPLATE = """
         <input type="text" id="town" name="town">
         <input type="submit" value="Search">
     </form>
-    {% if town_info %}
-        <h3>Town Information:</h3>
-        <p>{{ town_info }}</p>
+    {% if request.method == 'POST' %}
+        {% if results is not none and results %}
+            <h3>Town Information:</h3>
+            <table border="1">
+                <tr>
+                    <th>Type (Вид) </th>
+                    <th>Name (Име) </th>
+                    <th>Region (Област) </th>
+                    <th>Municipality (Община)</th>
+                    <th>Town hall (Име на кметството)</th>
+                </tr>
+                {% for row in results %}
+                <tr>
+                    <td>{{ row[0] }}</td>
+                    <td>{{ row[1] }}</td>
+                    <td>{{ row[2] }}</td>
+                    <td>{{ row[3] }}</td>
+                    <td>{{ row[4] }}</td>
+                </tr>
+                {% endfor %}
+            </table>
+        {% else %}
+            <p>No results found.</p>
+        {% endif %}
+    {% endif %}
+
+    {% if request.method == 'POST' %}
+        {% if regions_count is not none %}
+        <h3>Total Number of Regions: {{ regions_count }} </h3>
+        {% endif %}
     {% endif %}
 </body>
 </html>
@@ -33,24 +60,46 @@ HTML_TEMPLATE = """
 
 @app.route('/', methods=['GET', 'POST'])
 def search_town():
-    town_info = None
+    results = None
+    regions_count = None
+
     if request.method == 'POST':
         town_name = request.form['town']
+        print(town_name)
         conn = psycopg2.connect(**conn_params)
         cur = conn.cursor()
-        cur.execute("SELECT s.type, s.name, r.name, m.name FROM settlements as s JOIN regions AS r ON s.region_code = r.code JOIN municipalities as m ON s.municipalities_code = m.code WHERE s.name = %s", (town_name,))
-        result = cur.fetchone()
-        print(result)
-        type = result[0]
-        town_namee = result[1]
-        obl = result[2]
-        obsht = result[3]
+        # cur.execute("SELECT s.type, s.name, r.name, m.name FROM settlements as s JOIN regions AS r ON s.region_code = r.code JOIN municipalities as m ON s.municipalities_code = m.code WHERE s.name = %s", (town_name,))
+        cur.execute("""SELECT s.type, s.name, r.name, m.name, COALESCE(th.name, '-') 
+                    FROM settlements as s 
+                    JOIN regions AS r ON s.region_code = r.code 
+                    JOIN municipalities as m ON s.municipalities_code = m.code 
+                    LEFT JOIN town_halls AS th ON s.town_hall_code = th.code 
+                    WHERE s.name = %s""", (town_name,))
+        results = cur.fetchall()
         cur.close()
-        if result:
-            town_info = f"Вид: {type}, {town_namee}, област: {obl}, община: {obsht}"
-        else:
-            town_info = "Town not found."
-    return render_template_string(HTML_TEMPLATE, town_info=town_info)
+        cur = conn.cursor()
+
+        cur.execute("SELECT COUNT(name) FROM regions")
+        regions_count = cur.fetchone()[0]
+
+        print(len(results))
+        print(regions_count)
+        cur.close()
+        conn.close()
+        print(results)
+        for row in results:
+            type = row[0]
+            town_namee = row[1]
+            obl = row[2]
+            obsht = row[3]
+            # kmets = row[4]
+            if row:
+                towns_info = f"Вид: {type}, {town_namee}, област: {obl}, община: {obsht}"
+                print(towns_info)
+            else:
+                towns_info = "Town not found."
+
+    return render_template_string(HTML_TEMPLATE, results=results, regions_count=regions_count)
 
 if __name__ == '__main__':
     app.run(debug=True)
